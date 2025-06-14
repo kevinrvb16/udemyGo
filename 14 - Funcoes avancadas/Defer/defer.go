9b79276b02dcf2995437847d24888344908f0d4a
kvbernal <kevin.vbernal@mercadolivre.com>
2025-06-14 14:53:27 -0300

    learning defer go

---
+++ 14 - Funcoes avancadas/Defer/defer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+func funcao1() {
+	fmt.Println("Executando a funcao 1")
+}
+
+func funcao2() {
+	fmt.Println("Executando a funcao 2")
+}
+func alunoEstaAprovado(n1, n2 float32) bool {
+	defer fmt.Println("Media calculada. Resultado sera retornado")
+	fmt.Println("Entrando na funcao para verificar se o aluno esta aprovado")
+
+	media := (n1 + n2)/2
+	if media >= 6 {
+		return true
+	}
+	return false
+}
+func main() {
+	fmt.Println(alunoEstaAprovado(7, 8))
+}
